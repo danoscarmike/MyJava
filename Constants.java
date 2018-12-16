beb7ad3d84ec55984000fb1728437cde56025de8
Dan O'Meara <omeara.dan@gmail.com>
2018-12-15 23:06:42 -0800

    before comments

---
+++ Constants.java
@@ -0,0 +1,16 @@
+class Constants {
+  public static void main(String[] args) {
+    final int TOUCHDOWN = 6;
+    final int CONVERSION = 1;
+    final int FIELDGOAL = 3;
+
+    int td, pat, fg, total;
+
+    td = 4 * TOUCHDOWN;
+    pat = 3 * CONVERSION;
+    fg = 3 * FIELDGOAL;
+    total = (td + pat + fg);
+
+    System.out.println("Score: " + total);
+  }
+}
