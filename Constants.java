3e9c121cf4b80282678074bd90e491998ca3a75a
Dan O'Meara <omeara.dan@gmail.com>
2018-12-15 23:38:56 -0800

    getting started

--- Constants.java
+++ Constants.java
@@ -1,16 +1,24 @@
+/*
+  Practice constant variables.
+*/
+
 class Constants {
   public static void main(String[] args) {
+    // A touchdown is six points etc etc.
     final int TOUCHDOWN = 6;
     final int CONVERSION = 1;
     final int FIELDGOAL = 3;
 
+    // Initialize subtotals and grand total.
     int td, pat, fg, total;
 
+    // Calculate points scored.
     td = 4 * TOUCHDOWN;
     pat = 3 * CONVERSION;
     fg = 3 * FIELDGOAL;
     total = (td + pat + fg);
 
+    // Output the total.
     System.out.println("Score: " + total);
   }
 }
---
+++ DataTypes.java
@@ -0,0 +1,15 @@
+class DataTypes {
+  public static void main(String[] args) {
+    char letter = 'D';
+    String title = "Program Manager";
+    int number = 7;
+    float decimal = 3.14f;
+    boolean result = false;
+
+    System.out.println("First initial is " + letter);
+    System.out.println("My job is " + title);
+    System.out.println("Favorite number: " + number);
+    System.out.println("Love to eat: " + decimal);
+    System.out.println("True: " + result);
+  }
+}
