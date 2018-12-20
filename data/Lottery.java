4560a37c72dbbab98b95cf28698a372b07008208
Dan O'Meara <omeara.dan@gmail.com>
2018-12-20 09:23:24 -0800

    Only shuffle first 6

--- data/Lottery.java
+++ data/Lottery.java
@@ -1,11 +1,11 @@
 class Lottery {
   public static void main(String[] args) {
     int[] allNumbers = new int[59];
-    for (int h=0; h<59; h++) {
+    for (int h=0; h<allNumbers.length; h++) {
       allNumbers[h] = h+1;
     }
     
-    for (int i=0; i<allNumbers.length; i++) {
+    for (int i=0; i<6; i++) {
       int number = (int) Math.floor(Math.random() * 59);
       int temp = allNumbers[i];
       allNumbers[i] = allNumbers[number];
